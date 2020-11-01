a44ef50a49fc63c796119b4997133db44d841f45
hija <hilko.j@wwu.de>
2020-11-01 15:34:55 +0100

    Backend Server works as expected (only upload)

--- backend/efs-server.go
+++ backend/efs-server.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/Terry-Mao/goconf"
 )
@@ -17,7 +18,31 @@ type EFSConfig struct {
 }
 
 type EFSUpload struct {
-	EncryptedFile string
+	// This is what we get from the client as JSON
+	EncryptedFile     string // The encrypted file itself
+	EncryptedFileName string // The encrypted file name
+}
+
+type EFSConfiguration struct {
+	// This is how we save on the server to save some space
+	EncryptedFileName string // The encrypted file name
+	EncryptedFilePath string // The encrypted file itself, stored seperately
+}
+
+type EFSDownload struct {
+	// This is how the client asks for a download as JSON
+	// Additionally this is our response to an upload request
+	EFSConfigurationName string // The efsconfiguration name
+}
+
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method is not supported.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// First we need to look up if the target config even exists
+
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,7 +52,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tmpfile, err := ioutil.TempFile(getConfig().Storage_path, "enc_")
+	tmpfile, err := ioutil.TempFile(getConfig().Storage_path, "*")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,9 +67,32 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tmpfile.WriteString(efsupload.EncryptedFile)
+	// We decode the base64 file, so we can save some disk space
+	base64DecodedFile, _ := base64.StdEncoding.DecodeString(efsupload.EncryptedFile)
+	tmpfile.Write(base64DecodedFile)
+	// io.WriteString(w, tmpfile.Name())
 
-	io.WriteString(w, tmpfile.Name())
+	// Create overview file
+	efsconfiguration := EFSConfiguration{}
+	efsconfiguration.EncryptedFileName = efsupload.EncryptedFileName
+	efsconfiguration.EncryptedFilePath = tmpfile.Name()
+
+	// Write configuration to jsonfile
+	jsonFileName := tmpfile.Name() + ".json"
+	jsonEFSConfiguration, err := json.Marshal(efsconfiguration)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ioutil.WriteFile(jsonFileName, jsonEFSConfiguration, 0644)
+
+	// Write response to client
+	efsdownload := EFSDownload{}
+	efsdownload.EFSConfigurationName = filepath.Base(tmpfile.Name())
+	efsdownloadBytes, err := json.Marshal(efsdownload)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(efsdownloadBytes)
 }
 
 func getConfig() *EFSConfig {
